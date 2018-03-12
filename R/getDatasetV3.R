aa950a4ffbc2c5be13cc623a00670cce1696110f
Bryan Schreiner <bryanschreiner@gmail.com>
2018-03-12 16:54:32 -0600

    adding stuff

---
+++ R/getDatasetV3.R
@@ -0,0 +1,13 @@
+#' @title Get a summary list of datasets available to the API user (V3)
+#' @description Returns a table with meta-data about all the datasets. This function uses the V3 version of the API.
+#' @param apiKey You can find the API key in the settings after logging into Numetric
+#' @export
+getDatasetsV3 <- function(apiKey){
+  r <- GET("https://api.numetric.com/v3/dataset",
+           add_headers("Authorization" = apiKey,
+                       "Content-Type" = "application/json")
+           #verbose()
+  )
+  response <- httr::content(r, as = "text") # Saves what was returned as raw text with all the encodings
+  response <- fromJSON(response) # Converts what was returned to a dataframe
+}
